8953f24bccbdcb53d6262f20e6f9f5db6ecafba3
m <m@MacBook-Pro-alkhas-b-mustafa.local>
2026-01-16 02:57:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/")
+async def handle_request(request: Request):
+    pyload = await request.json()
+    intent = pyload['queryResult']['intent']['displayName']
+    parameters = pyload['queryResult']['parameters']
+    #output_context = ['queryResult']['outputContexts']
+
+    if intent == "track.order - context: ongoing-tracking":
+       return JSONResponse(content={
+           "fulfillmentText": "Recieved in the backend"
+       })
+    return JSONResponse(content={
+        "fulfillmentText": "Not Recieved in the backend"
+    })
