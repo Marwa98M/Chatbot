5b0a527f26c6340329222915ba210933caab826e
m <m@MacBook-Pro-alkhas-b-mustafa.local>
2026-01-16 04:20:55 +0300

    Updated webhook

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
-
+import db_helper
 app = FastAPI()
 
 
@@ -17,10 +17,18 @@ async def handle_request(request: Request):
     #output_context = ['queryResult']['outputContexts']
 
     if intent == "track.order - context: ongoing-tracking":
-        fulfillment_text = f"Received order {intent} in the backend"
-        return JSONResponse(content={
-            "fulfillmentText": fulfillment_text
-        })
+        track_order(parameters)
+
+def track_order(parameters: dict):
+    order_id = parameters['order_id']
+    order_status = db_helper.get_order_status(order_id)
+
+    if order_status:
+        fulfillment_text = f"The order status for order id: {order_id} is: {order_status}"
+    else:
+        fulfillment_text = f"No order found with order id: {order_id}"
+
+
     return JSONResponse(content={
-        "fulfillmentText": "Not Recieved in the backend"
+        "fulfillmentText": fulfillment_text
     })
