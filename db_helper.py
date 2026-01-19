9621583ec32ba6fbd2012d34917c775b4a84371b
m <m@MacBook-Pro-alkhas-b-mustafa.local>
2026-01-19 20:57:08 +0300

    error fix

--- db_helper.py
+++ db_helper.py
@@ -1,21 +1,33 @@
-# Author: Dhaval Patel. Codebasics YouTube Channel
-import mysql.connector
-global cnx
-
+from dotenv import load_dotenv
 import os
+import mysql.connector
 
-cnx = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT")),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    ssl_ca=None
-)
+load_dotenv()
+print("DB Host:", os.getenv("DB_HOST"))
+
+
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+    )
+
+# cnx = mysql.connector.connect(
+#     host=os.getenv("DB_HOST"),
+#     port=int(os.getenv("DB_PORT")),
+#     user=os.getenv("DB_USER"),
+#     password=os.getenv("DB_PASSWORD"),
+#     database=os.getenv("DB_NAME"),
+#     ssl_ca=None
+# )
 
 
 # Function to call the MySQL stored procedure and insert an order item
 def insert_order_item(food_item, quantity, order_id):
+    cnx = get_db()
     try:
         # Initializing the cursor, Calling the stored procedure, Committing the changes, Closing the cursor
         cursor = cnx.cursor()
@@ -32,12 +44,13 @@ def insert_order_item(food_item, quantity, order_id):
 
     except Exception as e:
         print(f"An error occurred: {e}")
-        cnx.rollback() # Rollback changes if necessary
+        cnx.rollback()  # Rollback changes if necessary
         return -1
 
 
 # Function to execute the SQL query to insert a record into the order_tracking table
 def insert_order_tracking(order_id, status):
+    cnx = get_db()
     cursor = cnx.cursor()
     insert_query = "INSERT INTO order_tracking (order_id, status) VALUES (%s, %s)"
     cursor.execute(insert_query, (order_id, status))
@@ -47,37 +60,39 @@ def insert_order_tracking(order_id, status):
 
 # Function to execute the SQL query to get the total order price
 def get_total_order_price(order_id):
+    cnx = get_db()
     cursor = cnx.cursor()
     query = f"SELECT get_total_order_price({order_id})"
     cursor.execute(query)
-    result = cursor.fetchone()[0] # Fetching the result
+    result = cursor.fetchone()[0]  # Fetching the result
     cursor.close()
     return result
 
 
 # Function to executing the SQL query to get the next available order_id.
 def get_next_order_id():
+    cnx = get_db()
     cursor = cnx.cursor()
     query = "SELECT MAX(order_id) FROM orders"
     cursor.execute(query)
-    result = cursor.fetchone()[0]     # Fetching the result
+    result = cursor.fetchone()[0]  # Fetching the result
     cursor.close()
-    if result is None: # if it is empty
+    if result is None:  # if it is empty
         return 1
     else:
         print(result)
         return result + 1
 
 
-
 # Function for executing the SQL query to fetch the order status from the order_tracking table
 def get_order_status(order_id):
+    cnx = get_db()
     try:
         cursor = cnx.cursor()
-        #query = f"SELECT status FROM order_tracking WHERE order_id = {order_id}"
+        # query = f"SELECT status FROM order_tracking WHERE order_id = {order_id}"
         query = "SELECT status FROM order_tracking WHERE order_id = %s"
         cursor.execute(query, (order_id,))
-        result = cursor.fetchone() # Fetching the result
+        result = cursor.fetchone()  # Fetching the result
         cursor.close()
         return result[0] if result else None
 
@@ -88,13 +103,11 @@ def get_order_status(order_id):
         print(f"Error: {e}")
         return None
 
-
-
 #
 # if __name__ == "__main__":
-    # get_order_status()
-    # print(get_total_order_price(56))
-    # insert_order_item('Samosa', 3, 99)
-    # insert_order_item('Pav Bhaji', 1, 99)
-    # insert_order_tracking(99, "in progress")
-    # print(get_next_order_id())
+# get_order_status()
+# print(get_total_order_price(56))
+# insert_order_item('Samosa', 3, 99)
+# insert_order_item('Pav Bhaji', 1, 99)
+# insert_order_tracking(99, "in progress")
+# print(get_next_order_id())
